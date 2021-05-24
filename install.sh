d2fe0d3e1668ccc61193e7c42184672cfa92d036
bright.jiang <bright.jiang@amd.com>
2021-05-24 16:15:19 +0800

    add linux soft link for chia cli

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ cp cfg/*  /etc/chia/
 cp supervisor/* /etc/supervisor/conf.d/
 mkdir -p /var/log/chia/
 chmod 666 /var/log/chia
+ln -sfv /usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon/chia  /usr/bin/chia
