f327cf9c4c1eae9b5486696bbf692c5b980ebc22
godvmxi <godvmxi@gmail.com>
2021-08-24 16:10:36 +0800

    update new chia pool key

--- plot_new.sh
+++ plot_new.sh
@@ -25,7 +25,7 @@ do
 	rm -rf ${RAM_DIR}/*  ${DST}/tmp/*
 	mkdir -p  ${RAM_DIR}  ${DST}/tmp/ ${DST}/dst/
 	#exit 0
-	time ${CWD}/chia-plotter-ram  -r 16 -t ${DST}/tmp/ -2 ${RAM_DIR}/   -d ${DST}/dst/  -c xch18fn2w49q4rh653vdtwm5ncewnwa32g3q5hzpkmauncszkh2xrdestu6j2v  -f 92375c767ac2138be741227023284e330c6bb7b7d8b0cacc170e5ce1ed19f064e672136d33e841ddafc7713c88a82e73
+	time ${CWD}/chia-plotter-ram  -r 18 -t ${DST}/tmp/ -2 ${RAM_DIR}/   -d ${DST}/dst/  -c xch1upd5ea9rcycpmaynj0sy34v53xjqwhy603yz24pms79thhcfnu8qtwap3x  -f 92375c767ac2138be741227023284e330c6bb7b7d8b0cacc170e5ce1ed19f064e672136d33e841ddafc7713c88a82e73
 	echo "Finsh the file -> "$i"-----------------------------"
 	date
 	echo
